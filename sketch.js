ff3eef5e337dd59cac28885581abb2585e8d282c
Davi <141150894+davimendesferraz@users.noreply.github.com>
2024-03-04 10:23:35 -0300

    .

---
+++ sketch.js
@@ -0,0 +1,28 @@
+function setup() {
+    createCanvas(1080, 720);
+    background("white");
+    cor = color(random(0, 255), random(0, 255), random(0, 255));
+    posicaoHorizontal = 200;
+    posicaoVertical = 200;
+  }
+  
+  function draw () {
+    fill(cor);
+    circle(posicaoHorizontal, posicaoVertical, 50);
+   
+    if(mouseX < posicaoHorizontal) {
+  posicaoHorizontal--;
+  }
+  if(mouseX > posicaoHorizontal) {
+  posicaoHorizontal++;
+  }
+    if(mouseY < posicaoVertical) {
+  posicaoVertical--;
+  }
+  if(mouseY > posicaoVertical) {
+  posicaoVertical++;
+  }
+    if(mouseIsPressed) {
+  cor = color(random(0, 255), random(0, 255), random(0, 255), random(0, 100));
+  }
+  }
